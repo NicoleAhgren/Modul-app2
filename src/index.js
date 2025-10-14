8be508d483a036514c5c0821ddea5ca3c071dcd5
Nicole Åhgren <na223jy@student.lnu.se>
2025-10-14 11:13:29 +0200

    Made a new repository with a clone of my module instead of npm package and added the code i had already written in my last one

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import QuizEngine from '../Modul/src/index.js'  // Din quiz-modul
+
+
+    const startBtn = document.getElementById('start-btn')
+    const quizContainer = document.getElementById('quiz-container')
+    const  startContainer = document.getElementById('start-container')
+    
+    quizContainer.style.display = 'none'
+
+    startBtn.addEventListener('click', () => {
+      startContainer.style.display = 'none'
+      quizContainer.style.display = 'block'
+    })
