4e60ee29fb0b43ed123a74918d45ce682256c268
Nicole Åhgren <na223jy@student.lnu.se>
2025-10-14 12:10:53 +0200

    la till frågor i min quizContainer

--- src/index.js
+++ src/index.js
@@ -7,8 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
   quizContainer.style.display = 'none'
 
+  const quiz = new QuizEngine()
+  quiz.addQuestion('What year did ABBA win Eurovision?', ['1972', '1973', '1974', '1975'], 2)
+  quiz.addQuestion('Which famous artist is known for creating the album The Wall?', ['David Bowie', 'Pink Floyd', 'The Who', 'Genesis'], 1)
+  quiz.addQuestion('Which composer is known for his symphonies and was deaf?', ['Mozart', 'Bach', 'Beethoven', 'Chopin'], 2)
+  quiz.addQuestion('Which composer wrote the opera Don Giovanni?', ['Mozart', 'Bach', 'Beethoven', 'Verdi'], 0)
+  quiz.addQuestion('Which band released the album Dark Side of the Moon?', ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'Queen'], 1)
+  quiz.addQuestion('Who is known as the "King of Pop"?', ['Elvis Presley', 'Michael Jackson', 'Prince', 'Freddie Mercury'], 1)
+
+
   startBtn.addEventListener('click', () => {
     startContainer.style.display = 'none'
     quizContainer.style.display = 'block'
+    quiz.startQuiz(3, 10)
+    nextQuestion()
   })
+
+  function nextQuestion() {
+    const question = quiz.getNextQuestion()
+    const questionText = document.getElementById('question-text')
+
+    questionText.textContent = question.text
+  }
 })
