13b1c39e4cf1ed27fbf73e88dc275a118c0b9e95
Nicole Åhgren <na223jy@student.lnu.se>
2025-10-14 11:43:24 +0200

    La till addEventListener för DOmContentLoaded

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import QuizEngine from '../Modul/src/index.js'  // Din quiz-modul
+import QuizEngine from '../Modul/src/index.js'
 
+document.addEventListener('DOMContentLoaded', () => {
+  const startBtn = document.getElementById('start-btn')
+  const quizContainer = document.getElementById('quiz-container')
+  const startContainer = document.getElementById('start-container')
 
-    const startBtn = document.getElementById('start-btn')
-    const quizContainer = document.getElementById('quiz-container')
-    const  startContainer = document.getElementById('start-container')
-    
-    quizContainer.style.display = 'none'
+  quizContainer.style.display = 'none'
 
-    startBtn.addEventListener('click', () => {
-      startContainer.style.display = 'none'
-      quizContainer.style.display = 'block'
-    })
+  startBtn.addEventListener('click', () => {
+    startContainer.style.display = 'none'
+    quizContainer.style.display = 'block'
+  })
+})
